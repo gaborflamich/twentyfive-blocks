1c682788891689b9d714f624a560907493dc9a2d
gabor.flamich <gabor.flamich@trendency.hu>
2025-01-02 07:50:46 +0100

    Tick item

--- functions.php
+++ functions.php
@@ -7,8 +7,26 @@ function twentyfive_blocks_init() {
 	register_block_type( __DIR__ . '/build/sample' );
 	register_block_type( __DIR__ . '/build/button' );
 	register_block_type( __DIR__ . '/blocks/ctaButton' );
+	register_block_type( __DIR__ . '/blocks/tickItem' );
 }
 add_action( 'init', 'twentyfive_blocks_init' );
+
+// ================================================================
+// Theme Customizations
+// ================================================================
+function theme_customizations() {
+	// Add custom category for custom block
+    add_filter('block_categories_all', function($categories, $post) {
+        $categories[] = array(
+            'slug' => 'wpflames',
+            'title' => __('WPFlames', 'twentytwentyfive'),
+            'icon' => null,
+        );
+        return $categories;
+    }, 10, 2);
+}
+add_action('init', 'theme_customizations');
+
 
 // ================================================================
 // Enqueue Admin Styles
